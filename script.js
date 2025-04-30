6bae231063bf53be751d9e15614c65a79765b3b2
VasavaVinesh07 <vasavavinesh713@gmail.com>
2025-04-30 13:47:55 +0530

    Add files via upload
    my script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Optional: You can add smooth scroll or any small interactive behavior here
+
+// Example: Scroll to top on logo click
+document.querySelector('.logo').addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+  
