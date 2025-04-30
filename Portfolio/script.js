6e672a97a1070056c78eb35b40f4203a69f55367
VasavaVinesh07 <vasavavinesh713@gmail.com>
2025-04-30 13:40:49 +0530

    Delete Portfolio/script.js

--- Portfolio/script.js
+++
@@ -1,7 +0,0 @@
-// Optional: You can add smooth scroll or any small interactive behavior here
-
-// Example: Scroll to top on logo click
-document.querySelector('.logo').addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  });
-  
